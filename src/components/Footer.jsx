4d11fffdbf7133b57ddbd43e51fd354defcae845
bhushan-chef <153006192+bhushan-chef@users.noreply.github.com>
2025-06-26 00:41:00 +0530

    first commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div className='flex items-center justify-between gap-4 py-3 mt-20'>
         <img src={assets.logo2} alt="" className='h-15 rounded-lg ' width={150}></img>
         <p className='flex-1 border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>
-            Copyright © 2023 All rights reserved | This template is made with <span className='text-red-500'>❤</span> by <a href="" className='text-blue-500 hover:underline' target='_blank'>Team AI</a>
+            Copyright © 2025 All rights reserved | This template is made with <span className='text-red-500'>❤</span> by <a href="" className='text-blue-500 hover:underline' target='_blank'>Team AI</a>
         </p>
         <div className='flex gap-2.5 '>
             <img src={assets.facebook_icon} alt="" className='h-8 inline-block mx-2'></img>
